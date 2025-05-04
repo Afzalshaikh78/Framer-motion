1bfdaaa5b244ad2a344e857115f58226e8312098
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2025-05-04 23:25:51 +0530

    Update Gestures.tsx

--- src/components/Gestures.tsx
+++ src/components/Gestures.tsx
@@ -3,17 +3,22 @@ import { motion } from "framer-motion";
 
 const Gestures = () => {
   return <div>
-    {/* <motion.div className="box" whileHover={{
+    <motion.div className="box" whileHover={{
       scale: 1.2,
       rotate: 99,
-      transition: { duration: 0.5 }
+      transition: { duration: 2 },
 
-    }}/> */}
 
-    <motion.div className="box" whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8,rotate:-180 }} 
+     
+
+    }}
+    
+    whileTap={{scale:0.3}}/>
+
+    {/* <motion.div className="box" whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8,rotate:-180 }} 
     transition={{type:'spring',stiffness:300} }/>
     
-      <h1>Gestures</h1>
+      <h1>Gestures</h1> */}
   </div>;
 };
 
