bb380c50ecbcbd9d339a27e75336dca76d35bd1e
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2025-05-04 23:21:15 +0530

    learn about Gestures

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import BouncingLoader from './components/BouncingLoader';
+import Gestures from "./components/Gestures";
 function App() {
   return (
   <div>
-    <BouncingLoader />
+      {/* <BouncingLoader /> */}
+      <Gestures/>
 
   </div>
 
---
+++ src/components/Gestures.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Gestures = () => {
+  return <div>
+    {/* <motion.div className="box" whileHover={{
+      scale: 1.2,
+      rotate: 99,
+      transition: { duration: 0.5 }
+
+    }}/> */}
+
+    <motion.div className="box" whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8,rotate:-180 }} 
+    transition={{type:'spring',stiffness:300} }/>
+    
+      <h1>Gestures</h1>
+  </div>;
+};
+
+export default Gestures;
