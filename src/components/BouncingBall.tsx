97c6b4852ace5fd91d9f0a51ab6e5268e8d2d308
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2025-06-22 21:01:55 +0530

    Update BouncingBall.tsx

--- src/components/BouncingBall.tsx
+++ src/components/BouncingBall.tsx
@@ -3,46 +3,33 @@ import { motion } from "framer-motion";
 
 const BouncingBall = () => {
   return (
-    <><motion.div
-      className="w-17 h-17 bg-yellow-500 rounded-full"
-      animate={{ y: [0, -200, 0] }}
-      transition={{
-        duration: 1,
-        ease: "easeInOut",
-        repeat: Infinity,
-        repeatType: "loop",
-      }}
-    >
-      <div className="w-full h-full flex items-center justify-center">
-
-
-      </div>
-
-
-
-    </motion.div>
-      
-      
+    <>
       <motion.div
-      className="w-17 h-17 bg-yellow-500 mt-3 rounded-full"
-      animate={{ y: [0, -200, 0] }}
-      transition={{
-        duration: 1,
-        ease: "easeInOut",
-        repeat: Infinity,
-        repeatType: "loop",
-      }}
-    >
-        <div className="w-full h-full flex items-center justify-center">
-          
+        className="w-17 h-17 bg-yellow-500 rounded-full"
+        animate={{ y: [0, -200, 0] }}
+        transition={{
+          duration: 1,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "loop",
+        }}
+      >
+        <div className="w-full h-full flex items-center justify-center"></div>
+      </motion.div>
 
-        </div>
-
-
-
-      </motion.div></>
-
-    
+      <motion.div
+        className="w-17 h-17 bg-yellow-500 mt-2 rounded-full"
+        animate={{ y: [0, -200, 0] }}
+        transition={{
+          duration: 1,
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatType: "loop",
+        }}
+      >
+        <div className="w-full h-full flex items-center justify-center"></div>
+      </motion.div>
+    </>
   );
 };
 
