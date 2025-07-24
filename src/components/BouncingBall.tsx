c59458c28a2924595bdcbc9a5864541997c6d43c
Afzalshaikh78 <afzalshaikh0771@gmail.com>
2025-07-24 22:48:01 +0530

    Update BouncingBall.tsx

--- src/components/BouncingBall.tsx
+++ src/components/BouncingBall.tsx
@@ -16,18 +16,6 @@ const BouncingBall = () => {
       >
         <div className="w-full h-full flex items-center justify-center"></div>
       </motion.div>
-      <motion.div
-        className="w-17 h-17 bg-yellow-500 rounded-full"
-        animate={{ y: [0, -200, 0] }}
-        transition={{
-          duration: 1,
-          ease: "easeInOut",
-          repeat: Infinity,
-          repeatType: "loop",
-        }}
-      >
-        <div className="w-full h-full flex items-center justify-center"></div>
-      </motion.div>
 
       <motion.div
         className="w-17 h-17 bg-yellow-500 mt-2 rounded-full"
